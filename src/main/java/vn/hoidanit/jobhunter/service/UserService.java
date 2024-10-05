a7149d72f76a5be360d12c1af81ce2053824c4cd
thanhdeptrai <taothichmi2009@gmail.com>
2024-10-05 19:07:20 +0900

    #32 chữa bài tập update user

--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -16,14 +16,17 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
+  // CREATE
   public User handleCreateUser(User user) {
     return this.userRepository.save(user);
   }
 
+  // DELETE
   public void handleDeleteUser(long id) {
     this.userRepository.deleteById(id);
   }
 
+  // GET BY ID
   public User handleGetUserById(long id) {
     Optional<User> userOptional = this.userRepository.findById(id);
     if (userOptional.isPresent()) {
@@ -32,20 +35,37 @@ public class UserService {
     return null;
   }
 
+  // GET ALL
   public List<User> handleGetAllUsers() {
     return this.userRepository.findAll();
   }
 
-  public User handleUpdateUser(User updatedUser) {
-    User existingUser = userRepository.findById(updatedUser.getId()).orElse(null);
+  // UPDATE
+  // CÁCH 1
+  // public User handleUpdateUser(User updatedUser) {
+  // User existingUser =
+  // this.userRepository.findById(updatedUser.getId()).orElse(null);
+  // if (existingUser != null) {
+  // existingUser.setName(updatedUser.getName());
+  // existingUser.setEmail(updatedUser.getEmail());
+  // existingUser.setPassword(updatedUser.getPassword());
 
+  // existingUser = this.userRepository.save(existingUser);
+  // }
+  // return existingUser;
+  // }
+
+  // CÁCH 2
+  public User handleUpdateUser(User updatedUser) {
+    User existingUser = this.handleGetUserById(updatedUser.getId());
     if (existingUser != null) {
       existingUser.setName(updatedUser.getName());
       existingUser.setEmail(updatedUser.getEmail());
       existingUser.setPassword(updatedUser.getPassword());
-      this.userRepository.save(existingUser);
-    }
 
+      // update
+      existingUser = this.userRepository.save(existingUser);
+    }
     return existingUser;
   }
 }
