73ae728b82ae34e7d53600ba16927218dc6d93db
thanhdeptrai <taothichmi2009@gmail.com>
2024-10-27 14:37:52 +0900

    #43

--- src/main/java/vn/hoidanit/jobhunter/util/FormatRestResponse.java
+++ src/main/java/vn/hoidanit/jobhunter/util/FormatRestResponse.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import jakarta.servlet.http.HttpServletResponse;
+import vn.hoidanit.jobhunter.domain.RestResponse;
 
 @ControllerAdvice
 public class FormatRestResponse implements ResponseBodyAdvice<Object> {
@@ -26,11 +27,20 @@ public class FormatRestResponse implements ResponseBodyAdvice<Object> {
       Class selectedConverterType,
       ServerHttpRequest request,
       ServerHttpResponse response) {
-
     HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
     int status = servletResponse.getStatus();
 
-    return body;
+    RestResponse<Object> res = new RestResponse<Object>();
+    res.setStatusCode(status);
+
+    if (status >= 400) {
+      return body;
+    } else {
+      res.setData(body);
+      res.setMessage("CALL API SUCCESS");
+    }
+
+    return res;
   }
 
 }
