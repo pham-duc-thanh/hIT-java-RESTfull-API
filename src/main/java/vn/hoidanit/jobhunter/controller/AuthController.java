03a274aa534e1d37ae8d1bf86869f1a94e6ab95a
thanhdeptrai <taothichmi2009@gmail.com>
2024-11-13 22:15:24 +0900

    #55

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.dto.LoginDTO;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AuthController {
 
   private final AuthenticationManagerBuilder authenticationManagerBuilder;
+  private final SecurityUtil securityUtil;
 
-  public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+  public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
     this.authenticationManagerBuilder = authenticationManagerBuilder;
+    this.securityUtil = securityUtil;
   }
 
   @PostMapping("/login")
@@ -29,6 +32,9 @@ public class AuthController {
 
     // xác thực người dùng => cần viết hàm loadUserByUsername
     Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+    // create a token
+    this.securityUtil.createToken(authentication);
     return ResponseEntity.ok().body(loginDTO);
   }
 
