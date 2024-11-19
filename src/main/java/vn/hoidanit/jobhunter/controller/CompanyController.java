8f15f70a8d7e202e790e09f04bee600a21e117f8
thanhdeptrai <taothichmi2009@gmail.com>
2024-11-19 22:26:03 +0900

    #67

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -0,0 +1,33 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.service.CompanyService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class CompanyController {
+
+  private final CompanyService companyService;
+
+  public CompanyController(CompanyService companyService) {
+    this.companyService = companyService;
+  }
+
+  @PostMapping("/companies")
+  public ResponseEntity<Company> createNewCompany(@Valid @RequestBody Company postManUser) {
+
+    Company thanhCompany = this.companyService.handleCreateCompany(postManUser);
+
+    // return ResponseEntity.ok(thanhCompany);
+    // return new ResponseEntity<>(thanhCompany, HttpStatus.CREATED);
+    return ResponseEntity.status(HttpStatus.CREATED).body(thanhCompany);
+  }
+
+}
