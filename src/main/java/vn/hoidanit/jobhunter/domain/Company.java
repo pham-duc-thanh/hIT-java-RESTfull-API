34422116c2d926c8550eeec97fb44a16991548c4
thanhdeptrai <taothichmi2009@gmail.com>
2024-11-20 22:00:04 +0900

    #70

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -2,6 +2,8 @@ package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 @Entity
 @Table(name = "companies")
@@ -33,6 +36,7 @@ public class Company {
 
   private String logo;
 
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+9")
   private Instant createdAt;
 
   private Instant updatedAt;
@@ -43,7 +47,9 @@ public class Company {
 
   @PrePersist
   public void handleBeforeCreate() {
-    this.createdBy = "thanh";
+    this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true ? SecurityUtil.getCurrentUserLogin().get()
+        : "";
+
     this.createdAt = Instant.now();
   }
 
