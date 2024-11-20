b339339d5ad22c5cb6a27c0036afb9f7f2669aac
thanhdeptrai <taothichmi2009@gmail.com>
2024-11-20 20:38:10 +0900

    #69

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -39,5 +40,11 @@ public class Company {
   private String createdBy;
 
   private String updatedBy;
+
+  @PrePersist
+  public void handleBeforeCreate() {
+    this.createdBy = "thanh";
+    this.createdAt = Instant.now();
+  }
 
 }
