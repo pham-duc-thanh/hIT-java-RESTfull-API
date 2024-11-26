0dc3755c93883091b924d91dd678d4b294a13324
thanhdeptrai <taothichmi2009@gmail.com>
2024-11-26 19:20:30 +0900

    #82

--- src/main/java/vn/hoidanit/jobhunter/domain/dto/ResLoginDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/dto/ResLoginDTO.java
@@ -1,15 +1,25 @@
 package vn.hoidanit.jobhunter.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResLoginDTO {
 
   private String accessToken;
+  private UserLogin user;
 
-  public String getAccessToken() {
-    return accessToken;
-  }
-
-  public void setAccessToken(String accessToken) {
-    this.accessToken = accessToken;
+  @Getter
+  @Setter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class UserLogin {
+    private long id;
+    private String email;
+    private String name;
   }
 
 }
