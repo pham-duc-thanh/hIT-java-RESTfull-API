dc42fdfc887f50d2cbdd7055fd89ea88df10e4b7
thanhdeptrai <taothichmi2009@gmail.com>
2024-12-03 22:56:31 +0900

    update #84

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -8,6 +8,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -16,10 +19,6 @@ import vn.hoidanit.jobhunter.domain.dto.LoginDTO;
 import vn.hoidanit.jobhunter.domain.dto.ResLoginDTO;
 import vn.hoidanit.jobhunter.service.UserService;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -32,18 +31,18 @@ public class AuthController {
   @Value("${hoidanit.jwt.refresh-token-validity-in-seconds}")
   private long refreshTokenExpiration;
 
-  public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil,
-      UserService userService) {
+  public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder,
+      SecurityUtil securityUtil, UserService userService) {
     this.authenticationManagerBuilder = authenticationManagerBuilder;
     this.securityUtil = securityUtil;
     this.userService = userService;
   }
 
   @PostMapping("/login")
-  public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
+  public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDto) {
     // Nạp input gồm username/password vào Security
     UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-        loginDTO.getUsername(), loginDTO.getPassword());
+        loginDto.getUsername(), loginDto.getPassword());
 
     // xác thực người dùng => cần viết hàm loadUserByUsername
     Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
@@ -53,7 +52,7 @@ public class AuthController {
     SecurityContextHolder.getContext().setAuthentication(authentication);
 
     ResLoginDTO res = new ResLoginDTO();
-    User currentUserDB = this.userService.handleGetUserByUsername(loginDTO.getUsername());
+    User currentUserDB = this.userService.handleGetUserByUsername(loginDto.getUsername());
     if (currentUserDB != null) {
       ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(
           currentUserDB.getId(),
@@ -65,14 +64,14 @@ public class AuthController {
     res.setAccessToken(access_token);
 
     // create refresh token
-    String refresh_token = this.securityUtil.createRefreshToken(loginDTO.getUsername(), res);
+    String refresh_token = this.securityUtil.createRefreshToken(loginDto.getUsername(), res);
 
     // update user
-    this.userService.updateUserToken(access_token, loginDTO.getUsername());
+    this.userService.updateUserToken(refresh_token, loginDto.getUsername());
 
     // set cookies
     ResponseCookie resCookies = ResponseCookie
-        .from("refresh_token1", refresh_token)
+        .from("refresh_token", refresh_token)
         .httpOnly(true)
         .secure(true)
         .path("/")
@@ -83,5 +82,4 @@ public class AuthController {
         .header(HttpHeaders.SET_COOKIE, resCookies.toString())
         .body(res);
   }
-
 }
