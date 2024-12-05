a235eb0951a72c851fcd40cafcd7cf39865380e4
thanhdeptrai <taothichmi2009@gmail.com>
2024-12-05 18:43:19 +0900

    update

--- src/main/java/vn/hoidanit/jobhunter/domain/dto/ResultPaginationDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/dto/ResultPaginationDTO.java
@@ -9,4 +9,13 @@ public class ResultPaginationDTO {
 
   private Meta meta;
   private Object result;
+
+  @Getter
+  @Setter
+  public static class Meta {
+    private int page;
+    private int pageSize;
+    private int pages;
+    private long total;
+  }
 }
