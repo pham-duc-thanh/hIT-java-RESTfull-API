f6fd9b44b5932da061b6fae811f89d6a52353e22
thanhdeptrai <taothichmi2009@gmail.com>
2024-12-05 18:53:10 +0900

    update

--- src/main/java/vn/hoidanit/jobhunter/domain/response/ResCreateUserDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/response/ResCreateUserDTO.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.jobhunter.domain.dto;
+package vn.hoidanit.jobhunter.domain.response;
 
 import java.time.Instant;
 
@@ -10,13 +10,14 @@ import vn.hoidanit.jobhunter.util.constant.GenderEnum;
 
 @Getter
 @Setter
-public class ResUpdateUserDTO {
+public class ResCreateUserDTO {
   private long id;
   private String name;
+  private String email;
   private GenderEnum gender;
   private String address;
   private int age;
 
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+9")
-  private Instant updatedAt;
+  private Instant createdAt;
 }
--- src/main/java/vn/hoidanit/jobhunter/domain/response/ResUpdateUserDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/response/ResUpdateUserDTO.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.jobhunter.domain.dto;
+package vn.hoidanit.jobhunter.domain.response;
 
 import java.time.Instant;
 
@@ -10,14 +10,13 @@ import vn.hoidanit.jobhunter.util.constant.GenderEnum;
 
 @Getter
 @Setter
-public class ResCreateUserDTO {
+public class ResUpdateUserDTO {
   private long id;
   private String name;
-  private String email;
   private GenderEnum gender;
   private String address;
   private int age;
 
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+9")
-  private Instant createdAt;
+  private Instant updatedAt;
 }
