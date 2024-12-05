e23084e59ad03f2edb933e652de337124ac2a488
thanhdeptrai <taothichmi2009@gmail.com>
2024-12-05 20:57:04 +0900

    #93

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -1,14 +1,15 @@
 package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -37,7 +38,6 @@ public class Company {
 
   private String logo;
 
-  // @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+9")
   private Instant createdAt;
 
   private Instant updatedAt;
@@ -45,6 +45,9 @@ public class Company {
   private String createdBy;
 
   private String updatedBy;
+
+  @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+  List<User> users;
 
   @PrePersist
   public void handleBeforeCreate() {
--- src/main/java/vn/hoidanit/jobhunter/domain/User.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/User.java
@@ -2,8 +2,6 @@ package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,6 +9,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -52,6 +52,10 @@ public class User {
   private Instant updatedAt;
   private String createdBy;
   private String updatedBy;
+
+  @ManyToOne
+  @JoinColumn(name = "company_id")
+  private Company company;
 
   @PrePersist
   public void handleBeforeCreate() {
